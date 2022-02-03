96792fa6d4f57a966be698662f860153846e9ba5
Swaakk <swathykrishna.thayyil@gmail.com>
2022-02-03 18:33:14 +0530

    git initial commit

---
+++ src/Api/Api.js
@@ -0,0 +1,2 @@
+export const API_KEY = "307d2a0064df9adfa3fa88f205fc82b4"
+export const API_ID = "b31532ca"
--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { API_ID, API_KEY } from "./Api/Api"
+import axios from "axios"
+
 
 function App() {
+  const url =  `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+  
+ function getReceipe(){
+
+ }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>hello world</h1>
       </header>
     </div>
   );
